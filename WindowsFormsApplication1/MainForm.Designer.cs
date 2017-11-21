3017c3bc45e0523c656413a4fde0f31b15697e29
tojoirinah <tojoirinah@gmail.com>
2017-11-21 19:58:44 +0100

    Ajout de la couche cross Layer

--- WindowsFormsApplication1/MainForm.Designer.cs
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroPnl = new MetroFramework.Controls.MetroPanel();
             this.SuspendLayout();
+            // 
+            // metroPnl
+            // 
+            this.metroPnl.HorizontalScrollbarBarColor = true;
+            this.metroPnl.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPnl.HorizontalScrollbarSize = 10;
+            this.metroPnl.Location = new System.Drawing.Point(2, 7);
+            this.metroPnl.Name = "metroPnl";
+            this.metroPnl.Size = new System.Drawing.Size(456, 290);
+            this.metroPnl.TabIndex = 0;
+            this.metroPnl.VerticalScrollbarBarColor = true;
+            this.metroPnl.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPnl.VerticalScrollbarSize = 10;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ClientSize = new System.Drawing.Size(465, 312);
+            this.Controls.Add(this.metroPnl);
             this.Name = "MainForm";
-            this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroPanel metroPnl;
+
     }
 }
--- WindowsFormsApplication1/MainForm.cs
+++ WindowsFormsApplication1/MainForm.cs
@@ -20,10 +20,14 @@ namespace WindowsFormsApplication1
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            // load size
             var w = Screen.PrimaryScreen.Bounds.Width;
             var h = Screen.PrimaryScreen.Bounds.Width;
             this.Location = new Point(0, 0);
             this.Size = new Size(w, h);
+
+            // Ajout panel dans usercontrol
+            
         }
     }
 }
